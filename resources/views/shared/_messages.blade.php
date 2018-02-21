3a26fab3c5404ffefe9a6147c671f1f3488db093
hanhan <hanhan@hanhandeMBP.lan>
2018-02-21 20:16:16 +0800

    Finish login and logout

--- resources/views/shared/_messages.blade.php
+++ resources/views/shared/_messages.blade.php
@@ -3,6 +3,9 @@
         <div class="flash-message">
             <p class="alert alert-{{ $msg }}">
                 {{ session()->get($msg) }}
+                @if(isset($user))
+                {{ $user->name }}
+                @endif
             </p>
         </div>
     @endif
